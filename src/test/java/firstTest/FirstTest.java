78a7ebaa6e6bfc5052c172b33034087a3dce9264
hariharan12081996 <harryjeens@gmail.com>
2020-01-20 10:24:36 +0530

    All files commit

---
+++ src/test/java/firstTest/FirstTest.java
@@ -0,0 +1,27 @@
+package firstTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class FirstTest {
+@Test
+public void validateFirstTest() throws IOException {
+	WebDriver wd = new FirefoxDriver();
+	wd.manage().window().maximize();
+	WebElement input = wd.findElement(By.name("q"));
+	input.sendKeys("chennai");
+	input.submit();
+	File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(src, new File("C:\\Users\\Chandra\\eclipse-workspace\\GitHubTest\\screenshot\\capture.jpg"));
+}
+}
